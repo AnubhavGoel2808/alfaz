bfb53bb8be1055a5dc3cc48e887193c3d04a6b91
AnubhavGoel2808 <47255993+AnubhavGoel2808@users.noreply.github.com>
2021-05-19 00:16:17 +0530

    modifications

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,8 +5,12 @@ import { auth ,db } from "../firebase";
 import '../style/profile.css'
 import FigureCaption from 'react-bootstrap/esm/FigureCaption';
 import Post from './post'
+import Popup from './popup'
 class Profile extends React.Component{
     state = {
+        isOpen:false,
+        isUser:false,
+        alreadyFollow:false,
         audio: [{ src:"https://www.computerhope.com/jargon/m/example.mp3", description:"this is a smaple audio"}
         ,{ src:"https://www.computerhope.com/jargon/m/example.mp3", description:"this is a smaple audio"},
         { src:"https://www.computerhope.com/jargon/m/example.mp3", description:"this is a smaple audio"}],
@@ -15,29 +19,12 @@ class Profile extends React.Component{
             image:'https://en.wikipedia.org/wiki/Image#/media/File:Image_created_with_a_mobile_phone.png'
            }
       }
-      // state = {
-      //   images: [],
-      //   userDetails: {}
-      // }
     
       componentWillMount() {
         this.fetchUserDetails()
         this.fetchPosts()
       }
     
-      // fetchPosts = async () => {
-      //   try {
-      //     const posts = await this.props.firebase.getUserPosts()
-      //     let images = posts.map(item => {
-      //       return item.postPhoto
-      //     })
-    
-      //     this.setState({ images })
-      //     // console.log(this.state.images)
-      //   } catch (error) {
-      //     console.log(error)
-      //   }
-      // }
       fetchPosts = async () => {
         const temp=[...this.state.audio];
         db.collection("post").where("handle", "==", "sal_vat_ion")
@@ -81,7 +68,9 @@ class Profile extends React.Component{
           console.log(error)
         }
       }
-    
+       togglePopup = () => {
+        this.setState({isOpen: !this.state.isOpen})
+      }
 
     render() {
       this.fetchUserDetails();
@@ -91,8 +80,19 @@ class Profile extends React.Component{
         return (
           <div className='main'>
             <section className='intro'>
-            <h1>{this.state.userDetails.handle}</h1>
-            
+            <div>
+                  <h1>{this.state.userDetails.handle}</h1>
+                  
+                    <button class={!this.state.alreadyFollow?"followToggle":"unfollow"} onClick={(e)=>{
+                        //{this.state.alreadyFollow ? alert('Are YOu sure?'):alert("you started following") }
+                        this.setState({
+                          alreadyFollow: !this.state.alreadyFollow,
+                          
+                        })
+                        
+                    }}>{this.state.alreadyFollow ? 'Unfollow' : 'Follow'}</button>
+            </div>
+
             <figure>
               
             <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--skK8N6A8--/c_fill,f_auto,fl_progressive,h_320,q_auto,w_320/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/511082/2860c4df-a0f4-4f4f-acb2-5e8bdb3bc673.jpg" className='avatar' alt={this.state.userDetails.name} >
@@ -121,7 +121,20 @@ class Profile extends React.Component{
                 </ul>
                 </div>
                 <div class="options">
-                 <button class='btn' style={{color:'cyan'}} >Edit</button>   
+                 <button class='btn' style={{color:'cyan'}} onClick={this.togglePopup}>Edit</button>   
+                 {this.state.isOpen && <Popup content={<>
+                          <b>Edit Details</b>
+                         <form class="EditForm">
+                           <input type="text" placeholder="name"></input><br></br>
+                           <input type="text" placeholder="name"></input><br></br>
+                           <input type="text" placeholder="name"></input><br></br>
+                           <input type="text" placeholder="name"></input><br></br>
+                           <input type="submit"></input>
+                         </form>
+                         
+                           </>}
+      handleClose={this.togglePopup}
+    />}
              <button class='btn' onClick={this.handleSignout} style={{color:'cyan'}}>Logout</button>
              </div>
             
